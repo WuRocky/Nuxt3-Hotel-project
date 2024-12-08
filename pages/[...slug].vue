54fdc4a904cec824c95d71d3d0c2d22752c78fd0
“Rocky <rockywu971@gmail.com>
2024-12-09 07:01:23 +0800

    Create templates and css

---
+++ pages/[...slug].vue
@@ -0,0 +1,18 @@
+<script setup>
+const route = useRoute();
+
+
+const page = ref(route.fullPath);
+</script>
+
+<template>
+  <div class="flex flex-col items-center py-24">
+    <h1 class="text-6xl font-semibold text-blue-600">404 Not Found</h1>
+    <p>你所請求的頁面：{{ page }}，不存在。</p>
+    <NuxtLink class="btn btn-primary" to="/">返回首頁</NuxtLink>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ pages/member/orders.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="flex flex-col items-center py-24">
+    <h1 class="text-6xl font-semibold text-blue-600">訂單查詢頁面</h1>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
