d9abace61fca7a59a149ce3e9b2b3117306f5a95
“Rocky <rockywu971@gmail.com>
2024-12-16 22:09:52 +0800

    change login

--- pages/account/login.vue
+++ pages/account/login.vue
@@ -1,49 +1,4 @@
 <script setup>
-  definePageMeta({
-    layout: 'account'
-  });
-
-</script>
-
-<template>
-  <div class="px-5 px-md-0">
-    <div class="mb-10">
-      <p class="mb-2 text-primary-100 fs-8 fs-md-7 fw-bold">
-        享樂酒店，誠摯歡迎
-      </p>
-      <h1 class="text-neutral-0 fw-bold">
-        立即開始旅程
-      </h1>
-    </div>
-
-    <Accountlogin />
-
-    <p class="mb-0 fs-8 fs-md-7">
-      <span class="me-2 text-neutral-0 fw-medium">沒有會員嗎？</span>
-      <NuxtLink
-        to="/account/register"
-        class="text-primary-100 fw-bold text-decoration-underline bg-transparent border-0"
-      >
-        <span>前往註冊</span>
-      </NuxtLink>
-    </p>
-  </div>
-</template>
-
-<style lang="scss" scoped>
-@import "bootstrap/scss/mixins/breakpoints";
-
-$grid-breakpoints: (
-  xs: 0,
-  sm: 576px,
-  md: 768px,
-  lg: 992px,
-  xl: 1200px,
-  xxl: 1400px,
-  xxxl: 1537px
-);
-
-</style><script setup>
 definePageMeta({
   layout: 'account'
 });
